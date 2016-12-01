1d163a964cfb4842579c11b43c34b6241d3bac2b
NahumRosillo <nahumrosillo92@gmail.com>
2016-12-01 11:54:54 +0100

    añadido la BD

---
+++ src/app/shared/bd/bd.ts
@@ -0,0 +1,10 @@
+import { User } from '../user';
+
+export interface BD {
+
+	addUser(user: User);
+
+	removeUser(user: User);
+
+	getUser(user: User): User;
+}
---
+++ src/app/shared/bd/memory.spec.ts
@@ -0,0 +1,29 @@
+/**
+ * Created by nahum on 26/11/16.
+ */
+
+import { TestBed, async } from '@angular/core/testing';
+import { User } from '../user';
+import { Memory } from './memory';
+
+let bd: Memory;
+let user: User;
+
+describe('Memory', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                Memory
+            ],
+        });
+
+        bd = new Memory();
+        user = new User();
+
+    });
+
+    it('se construye llamando al constructor predeterminado', async(() => {
+        
+        
+    }));
+});
