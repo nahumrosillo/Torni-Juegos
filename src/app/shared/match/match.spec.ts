185b3fd0005e178de9bc6c30006f10061140c816
Orrequia <orrequia8@gmail.com>
2016-12-02 18:29:14 +0100

    Finalizado la clase Match y sus test

--- src/app/shared/match/match.spec.ts
+++ src/app/shared/match/match.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { Match } from './match';
-import { User } from './../team/team'; 
+import { Team } from './../team/team';
+//import { User } from './../user/user'; 
 
 describe('Match', () => {
     beforeEach(() => {
@@ -13,16 +14,87 @@ describe('Match', () => {
 
     it('se construye llamando al constructor', async(() => {
 
-        let match = new Match();
+        let date = new Date();
+        date.setFullYear(2016, 12, 5);
+        let teamlocal = new Team(0, 5);
+        let teamvisitor = new Team(1, 5);
+
+        let match = new Match(date, teamlocal, teamvisitor);
 
         expect(match).toBeDefined;   
     }));
 
-    it('se construye llamando al constructor', async(() => {
+    it('se construye un partido con puntuación 0-0', async(() => {
 
-        let match = new Match();
+        let date = new Date();
+        date.setFullYear(2016, 12, 5);
+        let teamlocal = new Team(0, 5);
+        let teamvisitor = new Team(1, 5);
 
-        expect(match).toBeDefined;   
+        let match = new Match(date, teamlocal, teamvisitor);
+
+        expect(match.getScoreLocal).toEqual(0);
+        expect(match.getScoreLocal).toEqual(0);
+    }));
+
+    it('Están bien definido sus equipos y la fecha', async(() => {
+
+        let date = new Date();
+        date.setFullYear(2016, 12, 5);
+        let teamlocal = new Team(0, 5);
+        let teamvisitor = new Team(1, 5);
+
+        let match = new Match(date, teamlocal, teamvisitor);
+
+        expect(match.getLocalTeam).toEqual(teamlocal);
+        expect(match.getVisitorTeam).toEqual(teamvisitor);
+        expect(match.getStartDate).toEqual(date);
+    }));
+
+    it('Se asigna correctamente la puntuación de cada equipo y el ganador', async(() => {
+
+        let scoreLocal = 30;
+        let scoreVisitor = 29;
+
+        let date = new Date();
+        date.setFullYear(2016, 12, 5);
+        let teamlocal = new Team(0, 5);
+        let teamvisitor = new Team(1, 5);
+
+        let match = new Match(date, teamlocal, teamvisitor);
+
+        match.setScoreLocal = scoreLocal;
+        match.setScoreVisitor = scoreVisitor;
+
+        expect(match.getScoreLocal).toEqual(scoreLocal);
+        expect(match.getScoreVisitor).toEqual(scoreVisitor);
+        expect(match.getWinner).toEqual(teamlocal);
+    }));
+
+    it('No se puede modificar la puntuación una vez finalizado el partido', async(() => {
+
+        let scoreLocal = 30;
+        let scoreLocalFalse = 10;
+        let scoreVisitor = 29;
+        let scoreVisitorFalse = 11;
+
+        let date = new Date();
+        date.setFullYear(2016, 12, 5);
+        let teamlocal = new Team(0, 5);
+        let teamvisitor = new Team(1, 5);
+
+        let match = new Match(date, teamlocal, teamvisitor);
+
+        match.setScoreLocal = scoreLocal;
+        match.setScoreVisitor = scoreVisitor;
+
+        match.endMatch();
+
+        match.setScoreLocal = scoreLocalFalse;
+        match.setScoreVisitor = scoreVisitorFalse;
+
+        expect(match.getScoreLocal).toEqual(scoreLocal);
+        expect(match.getScoreVisitor).toEqual(scoreVisitor);
     }));
 
 
--- src/app/shared/match/match.ts
+++ src/app/shared/match/match.ts
@@ -8,7 +8,7 @@ export class Match {
 	private VisitorTeam: Team;
 	private finishedMatch: boolean = false;
 
-	constructor(startdate: Date, local: Team, visitor: Team)
+	constructor(startdate: Date, local: Team, visitor: Team) //OK
 	{
 		this.startDate = startdate;
 		this.LocalTeam = local;
@@ -17,6 +17,60 @@ export class Match {
 		this.scoreL = 0;
 		this.scoreV = 0;
 	}
+
+	endMatch() { //OK
+
+		this.finishedMatch = true;
+	}
+
+	// Getters
+	get getWinner(): Team { //OK
+		
+		if(this.scoreL > this.scoreV)
+			return this.LocalTeam;
+		else
+			return this.VisitorTeam;
+	}
+
+	get getLocalTeam(): Team {		//OK
+		
+		return this.LocalTeam;
+	}
+
+	get getVisitorTeam(): Team {	//OK
+		
+		return this.VisitorTeam;
+	}
+
+	get getScoreLocal(): number {	//OK
+
+		return this.scoreL;
+	}
+
+	get getScoreVisitor(): number {	//OK
+
+		return this.scoreV;
+	}
+
+	get getStartDate(): Date {		//OK
+
+		return this.startDate;
+	}
+
+
+	// Setters
+	set setScoreLocal(score: number) {  //OK
+
+		if(!this.finishedMatch)
+			this.scoreL = score;
+	}
+
+	set setScoreVisitor(score: number) { //OK
+		
+		if(!this.finishedMatch)
+			this.scoreV = score;
+	}
+	
 
 	
 
