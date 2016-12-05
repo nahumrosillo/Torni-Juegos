9d938e377d5d36c6a835ad9a3434d3343ccb2f3c
NahumRosillo <nahumrosillo92@gmail.com>
2016-12-05 19:10:57 +0100

    Added routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { NgModule }       from '@angular/core';
+
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginManagerComponent } from './manager/login-manager/login-manager.component';
+import { UserManagerComponent } from './manager/user-manager/user-manager.component';
+
+// Route Configuration
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login', component: LoginManagerComponent },
+  {path: 'usermanager', component: UserManagerComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRouting {}
