b9865ca041818d61af47568b1101f8c8831079d6
NahumRosillo <nahumrosillo92@gmail.com>
2016-12-13 16:24:51 +0100

    Implemented Observer pattern

--- src/app/manager/bd.service.ts
+++ src/app/manager/bd.service.ts
@@ -6,21 +6,24 @@ import { User, Rol, Genre } from '../shared/user/user';
 @Injectable()
 export class BDService {
 
-  get connect(): BD {
+  constructor() {
+    let userSuperAdmin = new User();
+    let date = new Date();
+    date.setFullYear(1992, 6, 2);
 
-  	let userSuperAdmin = new User();
-  	let date = new Date();
-  	date.setFullYear(1992, 6, 2);
-  	userSuperAdmin.setBirthDate = date;
-  	userSuperAdmin.setDni = '32079429H';
-  	userSuperAdmin.setName = 'Nahum';
-  	userSuperAdmin.setPassword = 'root';
-  	userSuperAdmin.setRol = Rol.SUPERADMIN;
-  	userSuperAdmin.setGenre = Genre.MALE;
-  	userSuperAdmin.setNick = 'root';
+    userSuperAdmin.setBirthDate = date;
+    userSuperAdmin.setDni = '32079429H';
+    userSuperAdmin.setName = 'Nahum';
+    userSuperAdmin.setPassword = 'root';
+    userSuperAdmin.setRol = Rol.SUPERADMIN;
+    userSuperAdmin.setGenre = Genre.MALE;
+    userSuperAdmin.setNick = 'root';
 
-  	Memory.getInstance.add(userSuperAdmin);
-  	
+    Memory.getInstance.add(userSuperAdmin);
+
+  }
+
+  get connect(): BD {  	
   	return Memory.getInstance;
   }
 }
