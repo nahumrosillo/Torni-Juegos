fd76ee8fbbb621bf2278fc4c51993737d5898d22
JsAntoPe <jsperegrina1@gmail.com>
2017-01-12 19:54:04 +0100

    "La api rest la estoy montando en memory y en api"

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,7 +4,7 @@ var mongojs = require('mongojs');
 var db = mongoose.connect('mongodb://localhost:27017/Mean'); 
  
 /* GET All Todos */
-router.get('/users', function(req, res, next) {
+router.get('/user', function(req, res, next) {
     db.todos.find(function(err, todos) {
         if (err) {
             res.send(err);
@@ -91,5 +91,93 @@ router.delete('/user/:id', function(req, res) {
     });
  
 });
+
+router.get('/game', function(req, res, next) {
+    db.todos.find(function(err, todos) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
  
+/* GET One Todo with the provided ID */
+router.get('/game/:id', function(req, res, next) {
+    db.todos.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function(err, todos) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+ 
+/* POST/SAVE a Todo */
+router.post('/game', function(req, res, next) {
+    var todo = req.body;
+    if (!todo.text || !(todo.isCompleted + '')) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    } else {
+        db.todos.save(todo, function(err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        })
+    }
+});
+ 
+/* PUT/UPDATE a Todo */
+router.put('/game/:id', function(req, res, next) {
+    var todo = req.body;
+    var updObj = {};
+ 
+    if (todo.isCompleted) {
+        updObj.isCompleted = todo.isCompleted;
+    }
+    if (todo.text) {
+        updObj.text = todo.text;
+    }
+ 
+    if (!updObj) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    } else {
+        db.todos.update({
+            _id: mongojs.ObjectId(req.params.id)
+        }, updObj, {}, function(err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(result);
+            }
+        });
+    }
+ 
+ 
+});
+ 
+/* DELETE a Todo */
+router.delete('/game/:id', function(req, res) {
+    db.todos.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    }, '', function(err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(result);
+        }
+    });
+ 
+});
+
 module.exports = router;
