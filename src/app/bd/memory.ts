e8f95c56402a3c762d7b581cacc5a6e3f42848bb
JsAntoPe <jsperegrina1@gmail.com>
2017-01-14 14:19:21 +0100

    Hay problemas en memory

--- src/app/bd/memory.ts
+++ src/app/bd/memory.ts
@@ -4,7 +4,7 @@ import { User } from '../shared/user/user';
 import { Game } from '../shared/game/game';
 import { Match } from '../shared/match/match';
 import { Tournament } from '../shared/tournament/tournament';
-
+import { Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Aggregator } from '../util/iterator/aggregator';
 import { IndexIterator } from '../util/iterator/indexIterator';
@@ -13,8 +13,7 @@ import { Iterator } from '../util/iterator/iterator';
 export class Memory implements BD
 {
 	private static instance: Memory;
-	API = 'http://localhost:4200';
-	private mapUser: Map<string, User>;
+	//private mapUser: Map<string, User>;
 	private mapGame: Map<string, Game>;
 
 	static get getInstance() {
@@ -22,15 +21,15 @@ export class Memory implements BD
     	if (this.instance === null || this.instance === undefined) {
 
             this.instance = new Memory();
-            this.instance.mapUser = new Map<string, User>();
+            //this.instance.mapUser = new Map<string, User>();
 			this.instance.mapGame = new Map<string, Game>();
         }
             return this.instance;
     }
 
-	private constructor() {}
+	//private constructor() {}
 
-	add(item: User | Game) {
+	/*add(item: User | Game) {
 
 		if (item instanceof User) {
 			this.mapUser.set(item.getNick, item);
@@ -99,21 +98,21 @@ export class Memory implements BD
 		});
 
 		return array;
-	}
-	/*
-	private constructor(http:Http) {}
+	}*/
+	
+	private constructor(private http:Http) {}
 
 	add(item: User | Game) {
 		var headers = new Headers();
         headers.append('Content-Type', 'application/json');
 
 		if (item instanceof User) {
-			return this.http.post('/api/User',json.stringify(item),{headers:headers})
+			return this.http.post('/api/User',JSON.stringify(item),{headers:headers})
 					.map(res=>res.json());
 		}
 
 		if (item instanceof Game) {
-			return this.http.post('/api/Game',json.stringify(item),{headers:headers})
+			return this.http.post('/api/Game',JSON.stringify(item),{headers:headers})
 					.map(res=>res.json());
 		}
 	}
@@ -121,23 +120,23 @@ export class Memory implements BD
 	remove(item: User | Game) {
 
 		if (item instanceof User) {
-			return this.http.delete('/api/user/'+id)
+			return this.http.delete('/api/User/'+item)
             		.map(res => res.json());
 		}
 
 		if (item instanceof Game) {
-			return this.http.delete('/api/game/'+id)
+			return this.http.delete('/api/Game/'+item)
             		.map(res => res.json());
 		}
 	}
 
-	getUser(item: User): User {
-		return this.http.get('/api/User',{item})
+	getUser(item: User){
+		return this.http.get('/api/User'+item)
 				.map(res=>res.json());
 	}
 
-	getGame(item: Game): Game {
-		return this.http.get('/api/Game',{item})
+	getGame(item: Game){
+		return this.http.get('/api/Game'+item)
 				.map(res=>res.json());
 	}
 
@@ -154,15 +153,15 @@ export class Memory implements BD
 	updateUser(item: User){
 		var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-		return this.http.post('/api/User',json.stringify(item),{headers:headers})
+		return this.http.post('/api/User',JSON.stringify(item),{headers:headers})
 				.map(res=>res.json());
 	}
 
 	updateGame(item: Game){
 		var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-		return this.http.post('/api/Game',json.stringify(item),{headers:headers})
+		return this.http.post('/api/Game',JSON.stringify(item),{headers:headers})
 				.map(res=>res.json());
 	}
-	*/
+	
 }
