af609fd9d15d2ded92e123d72512ad48c16a49b0
nahumrosillo <nahumrosillo92@gmail.com>
2017-01-22 23:23:20 +0100

    Version con mongoDD en Docker

--- src/app/systemManager.ts
+++
@@ -1,6 +0,0 @@
-import { BD } from './bd/bd';
-import { User} from './shared/user/user';
-
-export abstract class SystemManager  {
-	static dataBase: BD;
-}
--- src/app/util/observer/subject.ts
+++
@@ -1,8 +0,0 @@
-import { Observer } from './observer';
-
-
-export interface Subject {
-	registerObserver(observer: Observer);
-	removeObserver(observer: Observer);
-	notify();
-}
